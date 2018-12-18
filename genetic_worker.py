f789593f41855c20c3eafe1ffb9dbc429cca99ac
Chris Beeler <cbeeler@dal.ca>
2018-12-17 19:56:26 -0500

    Now keeps track of '_id' for each policy to avoid duplications

--- genetic_worker.py
+++ genetic_worker.py
@@ -2,7 +2,7 @@ import numpy as np
 import gym
 import gym.spaces
 from reactive_control import rc_gym
-from pymongo import MongoClient
+import pymongo
 from time import sleep
 import datetime
 
@@ -25,7 +25,7 @@ class Policy():
         self.W.append(w)
         self.B.append(b)
 
-        for i in range(1, self.hidden_units):
+        for i in range(1, len(self.hidden_units)):
             num_in = self.hidden_units[i-1]
             num_out = self.hidden_units[i]
             w = np.random.normal(scale = 1.0/(num_in*num_out), size = (num_in, num_out))
@@ -50,7 +50,7 @@ class Policy():
             self.W[0] += mut_rate * w
             self.B[0] += mut_rate * b
 
-            for i in range(1, self.hidden_units):
+            for i in range(1, len(self.hidden_units)):
                 num_in = self.hidden_units[i-1]
                 num_out = self.hidden_units[i]
                 w = np.random.normal(scale = 1.0/(num_in*num_out), size = (num_in, num_out))
@@ -66,52 +66,68 @@ class Policy():
             self.B[-1] += mut_rate * b
 
     def evaluate(self, state):
-        Y = np.tanh(np.matmul(state, self.W[i]) + self.B[i])
-        for i in range(len(self.W)):
+        Y = np.tanh(np.matmul(state, self.W[0]) + self.B[0])
+        for i in range(1, len(self.W)):
             Y = np.tanh(np.matmul(Y, self.W[i]) + self.B[i])
 
         Y = (Y + 1.0) / 2.0
         return Y * (self.a_bound[1] - self.a_bound[0]) + self.a_bound[0]
 
-db_name = '2h2o'                    # Name of database to use
 db_loc = 'coombs.science.uoit.ca'   # Location of MongoDB instance
 db_port = 2507                      # Port for MongoDB instance
 
-client = MongoClient(db_loc + ':' + str(db_port))
+client = pymongo.MongoClient(db_loc + ':' + str(db_port))
+parameter_table = client['parameters']
+
+params = parameter_table.posts.find_one()
+db_name = params['db_name']         # Name of database to use
+
 finished_table = client[db_name + '-finished']
 unfinished_table = client[db_name + '-unfinished']
 working_table = client[db_name + '-working']
 
-n_delete = 0
-while n_delete < 1:
-    n_unfinished = 0
-    while n_unfinished < 1:
-        sleep(1)
-        n_unfinished = unfinished_table.posts.count_documents({})
+while True:
+    n_delete = 0
+    while n_delete < 1:
+        n_unfinished = 0
+        while n_unfinished < 1:
+            sleep(1)
+            n_unfinished = unfinished_table.posts.count_documents({})
 
-    policy = unfinished.posts.find_one()
-    new_policy = {'gen': policy['gen'], 'name': policy['name'], 'id': policy['id'], 'seeds': policy['seeds'], 'start_time': datetime.datetime.utcnow()}
-    insert = working_table.posts.insert_one(new_policy)
-    delete = unfinished_table.posts.delete_one(policy)
-    n_delete = delete.deleted_count
+        policy = unfinished_table.posts.find_one()
+        new_policy = {'_id': policy['_id'], 'gen': policy['gen'], 'name': policy['name'], 'id': policy['id'], 'seeds': policy['seeds'], 'start_time': datetime.datetime.utcnow()}
 
-game = 'Pendulum-v0'
-env = gym.make(game)
-s = env.reset()
-shape = s.shape[0]
-hidden_units = np.array([1024])
-num_actions = env.action_space.shape[0]
-a_bound = [env.action_space.low, env.action_space.high]
-mut_rate = 0.05
-seeds = policy['seeds']
-policy = Policy(shape, hidden_units, num_actions, a_bound, mut_rate, seeds)
-reward = 0
-d = False
-while not d:
-    a = policy.evaluate(s)
-    s, r, d, _ = env.step(a)
-    reward += r
+        try:
+            insert = working_table.posts.insert_one(new_policy)
+        except pymongo.errors.DuplicateKeyError:
+            sleep(5)
+            try:
+                insert = working_table.posts.insert_one(new_policy)
+            except pymongo.errors.DuplicateKeyError:
+                pass
 
-finished_policy = {'gen': new_policy['gen'], 'name': new_policy['name'], 'id': new_policy['id'], 'seeds': new_policy['seeds'], 'score': reward}
-insert = finished_table.posts.insert_one(finished_policy)
-delete = working_table.posts.delete_one(new_policy)
+        print('Policy added')
+        sleep(5)
+        delete = unfinished_table.posts.delete_one(policy)
+        n_delete = delete.deleted_count
+
+    game = params['game']
+    env = gym.make(game)
+    s = env.reset()
+    shape = s.shape[0]
+    hidden_units = params['hidden_units']
+    num_actions = env.action_space.shape[0]
+    a_bound = [env.action_space.low, env.action_space.high]
+    mut_rate = params['mut_rate']
+    seeds = policy['seeds']
+    policy = Policy(shape, hidden_units, num_actions, a_bound, mut_rate, seeds)
+    reward = 0
+    d = False
+    while not d:
+        a = policy.evaluate(s)
+        s, r, d, _ = env.step(a)
+        reward += r
+
+    finished_policy = {'_id': new_policy['_id'], 'gen': new_policy['gen'], 'name': new_policy['name'], 'id': new_policy['id'], 'seeds': new_policy['seeds'], 'score': reward}
+    insert = finished_table.posts.insert_one(finished_policy)
+    delete = working_table.posts.delete_one(new_policy)
